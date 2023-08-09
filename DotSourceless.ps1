35a868d786ba5483318dd831a2be8610e6e06798
Dan Rivera <87451188+dnlrv@users.noreply.github.com>
2023-08-09 19:10:59 -0400

    Fixed url folder issue

--- DotSourceless.ps1
+++ DotSourceless.ps1
@@ -12,10 +12,12 @@ $functionfolder = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/dn
 # parsing out the html for just the scripts in the classfolder (regex skipping any _*.ps1 scripts)
 $ClassScripts = ($classfolder.RawContent -replace '\s','') | Select-String 'Classes\/(?!_)([a-zA-Z]+\-?[a-zA-Z]+\.ps1)' -AllMatches | foreach {$_.matches.Value -replace 'Classes\/(.*)','$1'} | Sort-Object -Unique
 $ClassScripts | Add-Member -MemberType NoteProperty -Name "ScriptType" -Value "Class"
+$ClassScripts | Add-Member -MemberType NoteProperty -Name FolderName -Value "Classes"
 
 # parsing out the html for just the scripts in the classfolder (regex skipping any _*.ps1 scripts)
 $FunctionScripts = ($functionfolder.RawContent -replace '\s','') | Select-String 'Functions\/(?!_)([a-zA-Z]+\-?[a-zA-Z]+\.ps1)' -AllMatches | foreach {$_.matches.Value -replace 'Functions\/(.*)','$1'} | Sort-Object -Unique
 $FunctionScripts | Add-Member -MemberType NoteProperty -Name "ScriptType" -Value "Function"
+$FunctionScripts | Add-Member -MemberType NoteProperty -Name FolderName -Value "Functions"
 
 # ArrayList to put all our scripts into
 $DotSourcelessScripts = New-Object System.Collections.ArrayList
@@ -29,7 +31,7 @@ $DotSourcelessScriptBlocks = New-Object System.Collections.ArrayList
 foreach ($script in $DotSourcelessScripts)
 {
     # format the uri
-    $uri = ("https://raw.githubusercontent.com/dnlrv/DotSourceless/main/{0}/{1}" -f $script.ScriptType, $script)
+    $uri = ("https://raw.githubusercontent.com/dnlrv/DotSourceless/main/{0}/{1}" -f $script.FolderName, $script)
 
     # new temp object for the ScriptBlock ArrayList
     $obj = New-Object PSCustomObject
